b39a42eddce1cd25814123ef6dcbb9002876303c
Syed Fareed Ahmed <syedahmed261@gmail.com>
2023-08-02 03:19:25 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+varaible "ami" {
+  default = "ami......."
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+varaible "bukcet_name" {
+  default = "tf_demo_bucket_test"
+}
+variable "dynamo_table_name" {
+  default = "tf_demo_table_name"
+}
